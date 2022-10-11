4fc797f057ca14254486e48e4dfa21c15d693a88
Maxime Ferriere <maxime.ferriere@student.vinci.be>
2022-10-11 15:41:36 +0200

    exo 2.11

--- exercices/module4/4a/src/App.js
+++ exercices/module4/4a/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -6,6 +7,15 @@ const App = () => {
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNumber] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
+  console.log("render", persons.length, "persons");
 
   const addSomeOne = (event) => {
     event.preventDefault();
